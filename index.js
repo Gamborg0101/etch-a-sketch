8ab11725d5fa155ce4634f83cc9632571527aa2d
Casper Gamborg <Gamborgc@gmail.com>
2025-03-24 10:03:39 +0100

    Grid is done. Continue to hoverEffect

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("Hello world!");
+
+let gridContainer = document.querySelector(".grid-container");
+let btnCreateGrid = document.querySelector(".create-grid");
+
+//btnCreateGrid.addEventListener("click", createGrid);
+
+
+
+function createGrid(size) {
+  for (let n = 0; n < size; n++) {
+    let gridDivX = document.createElement("div");
+    gridDivX.classList = `div${n}X`;
+    gridDivX.innerHTML = `This is grid X${n + 1}`;
+    gridDivX.classList.add("hover-effect")
+    gridContainer.appendChild(gridDivX);
+    for (let i = 0; i < size; i++) {
+      let gridDivY = document.createElement("div");
+      gridDivY.classList = `div${i}Y`;
+      gridDivY.innerHTML = `This is grid Y${i + 1}`;
+      gridDivX.appendChild(gridDivY);
+    }
+  }
+}
+
+
+
+createGrid(15);
