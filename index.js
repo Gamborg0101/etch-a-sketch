170e9cd1223763fc42bfbdb2e9f375ac10b9485c
Casper Gamborg <Gamborgc@gmail.com>
2025-03-24 12:55:35 +0100

    Done and refactored

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-console.log("Hello world!");
-
 let gridContainer = document.querySelector(".grid-container");
 let btnCreateGrid = document.querySelector(".create-grid");
-let allDivs = document.querySelectorAll("div");
+let btnResetGrid = document.querySelector(".reset-grid");
 
-//btnCreateGrid.addEventListener("click", createGrid);
+btnCreateGrid.addEventListener("click", () => {
+  let xValue = parseInt(prompt("How many pixels width would you like?"));
+  let yValue = parseInt(prompt("How many pixels height would you like?"));
+  createGrid(xValue, yValue);
+});
 
-function createGrid() {
-  //let xValue = parseInt(prompt("How many pixels width would you like?"));
-  //let yValue = parseInt(prompt("How many pixels height would you like?"));
+btnResetGrid.addEventListener("click", () => {
+  gridContainer.innerHTML = "";
+});
 
-  console.log(xValue);
-  console.log(typeof xValue == "number");
-
+function createGrid(xValue, yValue) {
   if (typeof xValue !== "number" || typeof yValue !== "number") {
     return "You need to enter a number";
   }
 
+  if (xValue > 100 || yValue > 100) {
+    let warningDiv = document.createElement("div");
+    warningDiv.classList.add("warningDiv");
+    warningDiv.innerText = "There is a maximum of 100x100 grid";
+    gridContainer.appendChild(warningDiv);
+
+    return;
+  }
+
+  gridContainer.innerHTML = ""; //Reset values in case user makes secound grid.
+
   for (let n = 1; n < xValue + 1; n++) {
     let gridDivX = document.createElement("div");
     gridDivX.classList = `div${n}X`;
-    gridDivX.innerHTML = `X${n} `;
-    gridDivX.classList.add("hover-effect");
+    //Learning: Should not x add value. It has to be invisible.
     gridContainer.appendChild(gridDivX);
+
     for (let i = 1; i < yValue + 1; i++) {
       let gridDivY = document.createElement("div");
       gridDivY.classList = `X${n}Y${i}`;
-      gridDivY.innerHTML = `Y${i}`;
+      gridDivY.innerHTML = ``;
+      gridDivY.style.width = "15px";
+      gridDivY.style.height = "15px";
+      gridDivY.style.border = "1px solid black";
+
+      gridDivY.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "red";
+      });
+
       gridDivX.appendChild(gridDivY);
     }
   }
 }
-
-allDivs.forEach((div) => {
-  div.addEventListener("mouseover", (e) => {
-    if (e.target !== gridContainer) {
-      console.log(e.target);
-      e.target.style.backgroundColor = "red";
-    }
-  });
-});
 
 createGrid();
 //Alternativ approach: mouseover could apply a new class instead, and then have a css rule for that.
