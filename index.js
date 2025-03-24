34e5670ae5a276e32cf022fa4b8d9baf24d56ae8
Casper Gamborg <Gamborgc@gmail.com>
2025-03-24 10:44:47 +0100

    Sketching is now possible, and works.

--- index.js
+++ index.js
@@ -2,27 +2,33 @@ console.log("Hello world!");
 
 let gridContainer = document.querySelector(".grid-container");
 let btnCreateGrid = document.querySelector(".create-grid");
+let allDivs = document.querySelectorAll("div");
 
 //btnCreateGrid.addEventListener("click", createGrid);
 
-
-
 function createGrid(size) {
-  for (let n = 0; n < size; n++) {
+  for (let n = 1; n < size + 1; n++) {
     let gridDivX = document.createElement("div");
     gridDivX.classList = `div${n}X`;
-    gridDivX.innerHTML = `This is grid X${n + 1}`;
-    gridDivX.classList.add("hover-effect")
+    gridDivX.innerHTML = `X${n} `;
+    gridDivX.classList.add("hover-effect");
     gridContainer.appendChild(gridDivX);
-    for (let i = 0; i < size; i++) {
+    for (let i = 1; i < size + 1; i++) {
       let gridDivY = document.createElement("div");
-      gridDivY.classList = `div${i}Y`;
-      gridDivY.innerHTML = `This is grid Y${i + 1}`;
+      gridDivY.classList = `X${n}Y${i}`;
+      gridDivY.innerHTML = `Y${i}`;
       gridDivX.appendChild(gridDivY);
     }
   }
 }
+console.log(gridContainer);
+allDivs.forEach((div) => {
+  div.addEventListener("mouseover", (e) => {
+    if (e.target !== gridContainer) {
+      console.log(e.target);
+      e.target.style.backgroundColor = "red";
+    }
+  });
+});
 
-
-
-createGrid(15);
+createGrid(9);
